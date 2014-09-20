6b836ff80ed6c98771c0d098ac746d44e2abfea9
Abraham Estrada <abraham.estrada@gmail.com>
2014-09-19 19:08:21 -0600

    Show items

--- html/app.js
+++ html/app.js
@@ -1,5 +1,20 @@
 window.app = angular.module('restaurant', []);
 
 app.controller('Cart', ['$scope', '$http', function($scope, $http) {
-    $scope.cart = 'Cart';
+    $scope.data = {
+        cart: {},
+        units: 0,
+        total: 0
+    };
+    $http({
+        method: 'GET',
+        url: 'http://demo9542161.mockable.io/v1/carts/c1659f57-b21e-49cc-ba8f-0dde54b3e161'
+    }).success(function(data, status, headers, config) {
+        $scope.data.cart = data;
+        for (var i = $scope.data.cart.order.items.length - 1; i >= 0; i--) {
+            var item = $scope.data.cart.order.items[i];
+            $scope.data.units += item.quantity;
+            $scope.data.total += item.total_price;
+        };
+    });
 }]);
